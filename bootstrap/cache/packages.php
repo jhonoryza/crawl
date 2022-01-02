a4c06fb6fda60ca7d5eb6613490ff2202bef0c7e
fajar sidik priatna <jardik.oryza@gmail.com>
2022-01-02 17:52:06 +0700

    add prod lock

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,27 +1,9 @@
 <?php return array (
-  'facade/ignition' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
-    ),
-    'aliases' => 
-    array (
-      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
-    ),
-  ),
   'fideloper/proxy' => 
   array (
     'providers' => 
     array (
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
-    ),
-  ),
-  'jhonoryza/api-gen' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Jhonoryza\\ApiGen\\ServiceProvider',
     ),
   ),
   'nesbot/carbon' => 
